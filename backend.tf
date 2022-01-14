02e87e856ad8e1a6722a739b0bf42b86ea196f6d
cristian080989 <cristian080989@gmail.com>
2022-01-14 13:57:36 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "sameeduddin-infra-19june21"
+    bucket = "stateterraformaws"
     key    = "aws-jenkins-remote.tfstate"
     region = "us-east-1"
   }
